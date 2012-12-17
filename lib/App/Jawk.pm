64eab8dff3dbc852df7bf04ae40661a3f7e77fc5
Josh Rabinowitz <joshr-cpan@joshr.com>
2012-12-17 01:04:14 +0000

    Import of JOSHR/App-Jawk-0.05 from CPAN.
    gitpan-cpan-distribution: App-Jawk
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         JOSHR/App-Jawk-0.05.tar.gz
    gitpan-cpan-author:       JOSHR
    gitpan-cpan-maturity:     released

---
+++ lib/App/Jawk.pm
@@ -0,0 +1,29 @@
+package App::Jawk;
+use strict;
+
+# this is just here for the documentation
+
+1;
+
+=head1 NAME
+
+App::Jawk -- Documentation for jawk
+
+=head1 SYNOPSIS
+
+See documentation for the script 'jawk'
+
+=head1 DESCRIPTION
+
+See documentation for the script L<jawk>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2010-2012 Josh Rabinowitz, All Rights Reserved.
+
+=head1 AUTHORS
+
+Josh Rabinowitz
+
+=cut    
+
